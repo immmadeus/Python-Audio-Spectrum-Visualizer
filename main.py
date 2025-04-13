03c9f50e34cebefada78f07fc3151120373a1b27
immmadeus <manishapatel8764@gmail.com>
2025-04-13 17:02:28 -0500

    Added option to change colors and FPS.

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import sys
 import pygame
-import audiovisualizer
 from audiovisualizer import update_spectrum
 import visualizergui
+import audiovisualizer
 
 def main():
     # Call the UI and get values
-    threshold, min_bar_height, smoothing_factor = visualizergui.run_ui()
-    pygame.display.set_caption(f"Audio Spectrum Visualizer - Input: {audiovisualizer.device_name}")
-
+    threshold, min_bar_height, smoothing_factor, fps, start_color, end_color = visualizergui.run_ui()
     try:
-        update_spectrum(threshold, min_bar_height, smoothing_factor)
+        pygame.display.set_caption(f"Audio Spectrum Visualizer - Input: {audiovisualizer.device_name}")
+        update_spectrum(threshold, min_bar_height, smoothing_factor,  fps, start_color, end_color)
     finally:
         pygame.quit()
         sys.exit()
